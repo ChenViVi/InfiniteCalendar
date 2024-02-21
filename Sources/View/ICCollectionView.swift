4da7300e21629fef676c9cc0cf274e77fc312ddd
ValoraChen <932942491@qq.com>
2024-02-21 15:12:40 +0800

    删除不会执行的方法

--- Sources/View/ICCollectionView.swift
+++ Sources/View/ICCollectionView.swift
@@ -11,20 +11,9 @@ import SwiftUI
 public class ICCollectionView: UICollectionView {
     public var registeredCells = [String]() // [identifiers]
     public var registeredSupplementaryClasses = [String:String]() // [kind:identifiers]
-    
-    public override func register(_ nib: UINib?, forCellWithReuseIdentifier identifier: String) {
-        super.register(nib, forCellWithReuseIdentifier: identifier)
-        registeredCells.append(identifier)
-    }
-    
     public override func register(_ cellClass: AnyClass?, forCellWithReuseIdentifier identifier: String) {
         super.register(cellClass, forCellWithReuseIdentifier: identifier)
         registeredCells.append(identifier)
-    }
-    
-    public override func register(_ nib: UINib?, forSupplementaryViewOfKind kind: String, withReuseIdentifier identifier: String) {
-        super.register(nib, forSupplementaryViewOfKind: kind, withReuseIdentifier: identifier)
-        registeredSupplementaryClasses[kind] = identifier
     }
     
     public override func register(_ viewClass: AnyClass?, forSupplementaryViewOfKind elementKind: String, withReuseIdentifier identifier: String) {
